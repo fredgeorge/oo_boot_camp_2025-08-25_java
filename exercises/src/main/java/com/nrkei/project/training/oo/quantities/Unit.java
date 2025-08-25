3c4b36be225470fc99d5086d59b96627b3bd19f7
Fred George <fredgeorge@acm.org>
2025-08-25 18:42:37 +0200

    Fred: Distance Quantities added; no check for cross Unit issues yet

--- exercises/src/main/java/com/nrkei/project/training/oo/quantities/Unit.java
+++ exercises/src/main/java/com/nrkei/project/training/oo/quantities/Unit.java
@@ -18,6 +18,15 @@ public final class Unit {
     public static final Unit QUART = new Unit(2, PINT);
     public static final Unit GALLON = new Unit(4, QUART);
 
+    public static final Unit INCH = new Unit();
+    public static final Unit FOOT = new Unit(12, INCH);
+    public static final Unit YARD = new Unit(3, FOOT);
+    public static final Unit FATHOM = new Unit(6, FOOT);
+    public static final Unit CHAIN = new Unit(22, YARD);
+    public static final Unit FURLONG = new Unit(10, CHAIN);
+    public static final Unit MILE = new Unit(8, FURLONG);
+    public static final Unit LEAGUE = new Unit(3, MILE);
+
     private final double baseUnitRatio;
 
     private Unit() {
@@ -30,6 +39,10 @@ public final class Unit {
 
     public Quantity s(double amount) {
         return new Quantity(amount, this);
+    }
+
+    public Quantity es(double amount) {
+        return s(amount);
     }
 
     double convertedAmount(double otherAmount, Unit other) {
