45d5245420a1df325588df096cbfb46e5b0ed27b
Fred George <fredgeorge@acm.org>
2025-08-25 17:40:36 +0200

    Fred: Quantity with equality and hashCode for like Units only

---
+++ exercises/src/main/java/com/nrkei/project/training/oo/quantities/Quantity.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+package com.nrkei.project.training.oo.quantities;
+
+import java.util.Objects;
+
+// Understands a specific measurement
+public final class Quantity {
+    private final double amount;
+    private final Unit unit;
+
+    public Quantity(double amount, Unit unit) {
+        this.amount = amount;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this == obj || obj instanceof Quantity && this.equals((Quantity) obj);
+    }
+
+    private boolean equals(Quantity quantity) {
+        return this.amount == quantity.amount && this.unit == quantity.unit;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount, unit);
+    }
+}
---
+++ exercises/src/main/java/com/nrkei/project/training/oo/quantities/Unit.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2025 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+package com.nrkei.project.training.oo.quantities;
+
+// Understands a specific metric
+public final class Unit {
+    public static final Unit TEASPOON = new Unit();
+    public static final Unit TABLESPOON = new Unit();
+    public static final Unit OUNCE = new Unit();
+    public static final Unit CUP = new Unit();
+    public static final Unit PINT = new Unit();
+    public static final Unit QUART = new Unit();
+    public static final Unit GALLON = new Unit();
+}
