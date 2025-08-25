b9450d474ed24f9444e379854387bd93c3b40cf3
Fred George <fredgeorge@acm.org>
2025-08-25 19:43:13 +0200

    Fred: Square as kind of Rectangle

--- tests/src/test/java/com/nrkei/project/training/oo/unit/RectangleTest.java
+++ tests/src/test/java/com/nrkei/project/training/oo/unit/RectangleTest.java
@@ -17,14 +17,17 @@ public class RectangleTest {
 
     @Test void area() {
         assertEquals(24.0, new Rectangle(4.0, 6.0).area());
+        assertEquals(36, Rectangle.square(6.0).area());
     }
 
     @Test void perimeter() {
         assertEquals(20.0, new Rectangle(4.0, 6.0).perimeter());
+        assertEquals(24, Rectangle.square(6.0).perimeter());
     }
 
     @Test void parameterRanges() {
         assertThrows(AssertionError.class, () -> new Rectangle(0, 6));
         assertThrows(AssertionError.class, () -> new Rectangle(4, 0));
+        assertThrows(AssertionError.class, () -> Rectangle.square(0));
     }
 }
