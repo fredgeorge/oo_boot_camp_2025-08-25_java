62d6c99e859b490f4244ee72b58146513dff263e
Fred George <fredgeorge@acm.org>
2025-08-26 14:40:03 +0200

    Fred: Graph with hopCount(); not necessarily minimum; duplicate code exists

--- exercises/src/main/java/com/nrkei/project/training/oo/graph/Node.java
+++ exercises/src/main/java/com/nrkei/project/training/oo/graph/Node.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 // Understands its neighbors
 public final class Node {
+    private static final int UNREACHABLE = -1;
     private final List<Node> neighbors = new ArrayList<>();
 
     public Node to(Node neighbor) {
@@ -20,6 +21,23 @@ public final class Node {
 
     public boolean canReach(Node destination) {
         return canReach(destination, noVisitedNodes());
+    }
+
+    public int hopCount(Node destination) {
+        var result = hopCount(destination, noVisitedNodes());
+        if (result == UNREACHABLE) throw new IllegalArgumentException("Destination is unreachable");
+        return result;
+    }
+
+    private int hopCount(Node destination, List<Node> visitedNodes) {
+        if (this == destination) return 0;
+        if (visitedNodes.contains(this)) return UNREACHABLE;
+        visitedNodes.add(this);
+        for (Node n : neighbors) {
+            var neighborHopCount = n.hopCount(destination, visitedNodes);
+            if (neighborHopCount != UNREACHABLE) return neighborHopCount + 1;
+        }
+        return UNREACHABLE;
     }
 
     private boolean canReach(Node destination, List<Node> visitedNodes) {
